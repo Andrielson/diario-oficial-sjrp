1d32857fafe4a2d37caf187c521ae1055b60d245
Andrielson Silva <andrielson@gmail.com>
2021-04-16 20:47:21 -0300

    Layout inicial da página de inscrição

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,74 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Diário Oficial SJRP</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Newsletter do <br />
+          Diário Oficial de <br />
+          <a href="https://riopreto.sp.gov.br" target="_blank">
+            São José do Rio Preto-SP
+          </a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Receba diariamente as publicações do diário oficial em seu e-mail
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.card}>
+            <form action="" method="post">
+              <p>
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  placeholder="Seu e-mail principal"
+                  autoFocus
+                  required
+                  className={styles.inputtext}
+                />
+              </p>
+              <p>
+                <button type="submit" className={styles.btnsend}>
+                  Inscrever-se
+                </button>
+              </p>
+            </form>
+          </div>
+          <div className={styles.disclaimer}>
+            <small>
+              <i>
+                Esta página não possui nenhuma relação com a prefeitura de São
+                José do Rio Preto-SP, sendo mantida por um desenvolvedor
+                independente para facilitar o acesso às publicações do diário
+                oficial.
+              </i>
+            </small>
+          </div>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <div>
+          <p>
+            <a
+              href="https://github.com/Andrielson"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              &copy; 2021 - Andrielson Silva
+            </a>
+          </p>
+        </div>
       </footer>
     </div>
-  )
+  );
 }
