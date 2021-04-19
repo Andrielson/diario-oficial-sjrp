16d51be10d0eab5fdd26997ea292a390a84dadd5
Andrielson Silva <andrielson@gmail.com>
2021-04-18 23:03:10 -0300

    Adição do MongoDB

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { Db, MongoClient } from "mongodb";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "../../util/mongodb";
 
-export default (_: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default async (_: NextApiRequest, res: NextApiResponse) => {
+  const conn: { client: MongoClient; db: Db } = await connectToDatabase();
+  const { client } = conn;
+
+  const isConnected = client.isConnected();
+  res.status(200).json({ name: "John Doe", isConnected });
+};
